b5200a7a52eb75ed4112cc1edba7279eba9348a9
Adel Belkhiri <adel.belkhiri@gmail.com>
2023-03-09 15:56:45 -0500

    dpdk: Initial commit
    This commit adds a plugin to handle DPDK traces. DPDK is a
    collection of libraries used to accelerate network packet
    processing. These traces can be generated using the Trace
    library from DPDK.
    
    The test trace dpdk_service_cores is available in
    tracecompass-test-traces in the following patch:
    https://git.eclipse.org/r/c/tracecompass/tracecompass-test-traces/+/201469
    
    Signed-off-by: Adel Belkhiri <adel.belkhiri@gmail.com>
    Signed-off-by: Arnaud Fiorini <fiorini.arnaud@gmail.com>

---
+++ analyses/org.eclipse.tracecompass.incubator.dpdk.core.tests/src/org/eclipse/tracecompass/incubator/dpdk/core/tests/stubs/DpdkTraceStub.java
@@ -0,0 +1,7 @@
+package org.eclipse.tracecompass.incubator.dpdk.core.tests.stubs;
+
+import org.eclipse.tracecompass.incubator.dpdk.core.trace.DpdkTrace;
+import org.eclipse.tracecompass.tmf.ctf.core.tests.stubs.ITmfTraceStub;
+
+public class DpdkTraceStub extends DpdkTrace implements ITmfTraceStub {
+}
---
+++ analyses/org.eclipse.tracecompass.incubator.dpdk.core/src/org/eclipse/tracecompass/incubator/internal/dpdk/core/lcore/analysis/DpdkLogicalCoreAnalysisModule.java
@@ -0,0 +1,60 @@
+/**********************************************************************
+ * Copyright (c) 2024 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+package org.eclipse.tracecompass.incubator.internal.dpdk.core.lcore.analysis;
+
+import static org.eclipse.tracecompass.common.core.NonNullUtils.checkNotNull;
+
+import java.util.Collections;
+
+import org.eclipse.tracecompass.incubator.dpdk.core.trace.DpdkTrace;
+import org.eclipse.tracecompass.tmf.core.analysis.requirements.TmfAbstractAnalysisRequirement;
+import org.eclipse.tracecompass.tmf.core.analysis.requirements.TmfAbstractAnalysisRequirement.PriorityLevel;
+import org.eclipse.tracecompass.tmf.core.analysis.requirements.TmfAnalysisEventRequirement;
+import org.eclipse.tracecompass.tmf.core.statesystem.ITmfStateProvider;
+import org.eclipse.tracecompass.tmf.core.statesystem.TmfStateSystemAnalysisModule;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * This analysis is tracking logical core state changes and service state
+ * changes in the state system. This is useful to see which cores of the machine
+ * is currently used by which service. The events related to this analysis are
+ * part of the Environment Abstraction Layer (EAL) module in DPDK.
+ *
+ * @author Adel Belkhiri
+ * @author Arnaud Fiorini
+ */
+public class DpdkLogicalCoreAnalysisModule extends TmfStateSystemAnalysisModule {
+
+    /** The ID of this analysis module */
+    public static final String ID = "org.eclipse.tracecompass.incubator.dpdk.lcore.analysis"; //$NON-NLS-1$
+    private final DpdkLogicalCoreEventLayout fLayout = new DpdkLogicalCoreEventLayout();
+
+    private final TmfAbstractAnalysisRequirement REQUIREMENT = new TmfAnalysisEventRequirement(ImmutableList.of(
+            fLayout.eventLcoreStateChange(), fLayout.eventServiceMapLcore()), PriorityLevel.AT_LEAST_ONE);
+
+    @Override
+    protected ITmfStateProvider createStateProvider() {
+        ITmfTrace trace = checkNotNull(getTrace());
+
+        if (trace instanceof DpdkTrace) {
+            return new DpdkLogicalCoreStateProvider(trace, fLayout, ID);
+        }
+
+        throw new IllegalStateException();
+    }
+
+    @Override
+    public Iterable<TmfAbstractAnalysisRequirement> getAnalysisRequirements() {
+        return Collections.singleton(REQUIREMENT);
+    }
+}
